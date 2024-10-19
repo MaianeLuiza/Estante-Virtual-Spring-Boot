0fff10afe41ed0e3622dd005d061138cf7bb7d11
MaianeLuiza <maianepaixao025@gmail.com>
2024-10-18 22:45:56 -0300

    feat: implementing validations for user registration

--- src/main/java/com/nam/estante_virtual/controller/UserController.java
+++ src/main/java/com/nam/estante_virtual/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.nam.estante_virtual.controller;
 
+import com.nam.estante_virtual.model.Role;
 import com.nam.estante_virtual.model.User;
+import com.nam.estante_virtual.repository.RoleRepository;
 import com.nam.estante_virtual.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +26,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @GetMapping("/new")
     public String addUser(Model model) {
         model.addAttribute("user", new User());
@@ -29,10 +36,22 @@ public class UserController {
     }
 
     @PostMapping("/save")
-    public String saveUser(@Valid User user, BindingResult result, RedirectAttributes attributes) {
+    public String saveUser(@Valid User user, BindingResult result, Model model, RedirectAttributes attributes) {
         if (result.hasErrors()){
             return "/public-new-user";
         }
+
+        User usr = userRepository.findByLogin(user.getLogin());
+        if (usr != null){
+            model.addAttribute("loginExists", "Login já cadastrado");
+            return "/public-new-user";
+        }
+
+        Role role = roleRepository.findByRole("USER");
+        List<Role> roles = new ArrayList<Role>();
+        roles.add(role);
+        user.setRoles(roles);
+
         userRepository.save(user);
         attributes.addFlashAttribute("message", "Usuário salvo com sucesso!");
         return "redirect:/user/new";
--- src/main/java/com/nam/estante_virtual/repository/UserRepository.java
+++ src/main/java/com/nam/estante_virtual/repository/UserRepository.java
@@ -5,4 +5,5 @@ import com.nam.estante_virtual.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByLogin(String login);
 }
